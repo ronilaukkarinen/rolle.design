4cd848ca61d7462e36db82b4dc2eb487fedfa840
Roni Laukkarinen <roni@laukkarinen.info>
2018-04-05 19:44:31 +0300

    Features, animations and stuff

---
+++ gulpfile.js
@@ -0,0 +1,180 @@
+/*
+
+REQUIRED STUFF
+==============
+*/
+
+var changed     = require('gulp-changed');
+var gulp        = require('gulp');
+var imagemin    = require('gulp-imagemin');
+var sass        = require('gulp-sass');
+var sourcemaps  = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+var notify      = require('gulp-notify');
+var prefix      = require('gulp-autoprefixer');
+var minifycss   = require('gulp-clean-css');
+var uglify      = require('gulp-uglify');
+var cache       = require('gulp-cache');
+var concat      = require('gulp-concat');
+var util        = require('gulp-util');
+var header      = require('gulp-header');
+var pixrem      = require('gulp-pixrem');
+var runSequence = require('run-sequence');
+var exec        = require('child_process').exec;
+
+/*
+
+ERROR HANDLING
+==============
+*/
+
+
+var handleError = function(task) {
+  return function(err) {
+
+      notify.onError({
+        message: task + ' failed, check the logs..',
+        sound: true
+      })(err);
+
+    util.log(util.colors.bgRed(task + ' error:'), util.colors.red(err));
+  };
+};
+
+/*
+
+FILE PATHS
+==========
+*/
+
+var imgSrc = 'src/images/*.{png,jpg,jpeg,gif}';
+var imgDest = 'dist/images';
+var sassSrc = 'src/sass/**/*.{sass,scss}';
+var sassFile = 'src/sass/base/global.scss';
+var cssDest = 'dist/css';
+var jsSrc = 'src/js';
+var jsDest = 'dist/js';
+var markupDist = 'src/*.php';
+var markupSrc = 'dist/*.php';
+var markupDest = 'dist';
+
+/*
+
+BROWSERSYNC
+===========
+*/
+
+gulp.task('browserSync', function() {
+
+    var files = [
+      imgDest + '/*.{png,jpg,jpeg,gif}',
+      jsSrc + '/**/*.js',
+      markupSrc,
+      markupDist
+    ];
+
+    browserSync.init(files, {
+        proxy: "rolle.design.test",
+        browser: "Google Chrome Canary",
+        notify: true
+    });
+});
+
+/*
+
+STYLES
+======
+*/
+
+gulp.task('styles', function() {
+  gulp.src(sassFile)
+
+  .pipe(sass({
+    compass: false,
+    bundleExec: true,
+    sourcemap: false,
+    style: 'compressed',
+    debugInfo: true,
+    lineNumbers: true,
+    errLogToConsole: true,
+    includePaths: [
+      'node_modules/',
+      // 'bower_components/',
+      // require('node-bourbon').includePaths
+    ],
+  }))
+
+  .on('error', handleError('styles'))
+  .pipe(prefix('last 3 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+  .pipe(minifycss({keepBreaks:false,keepSpecialComments:0,}))
+  .pipe(pixrem())
+  .pipe(gulp.dest(cssDest))
+  .pipe(browserSync.stream());
+
+  });
+
+/*
+
+IMAGES
+======
+*/
+
+gulp.task('images', function() {
+  var dest = imgDest;
+
+  return gulp.src(imgSrc)
+
+    .pipe(changed(dest)) // Ignore unchanged files
+    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))) //use cache to only target new/changed files, then optimize the images
+    .pipe(gulp.dest(imgDest));
+
+});
+
+/*
+
+SCRIPTS
+=======
+*/
+
+var currentDate   = util.date(new Date(), 'dd-mm-yyyy HH:ss');
+var pkg           = require('./package.json');
+var banner        = '/*! <%= pkg.name %> <%= currentDate %> - <%= pkg.author %> */\n';
+
+gulp.task('js', function() {
+
+      gulp.src(
+        [
+          'node_modules/jquery/dist/jquery.js',
+          'node_modules/what-input/dist/what-input.js',
+          jsSrc + '/scripts.js',
+        ])
+        .pipe(concat('all.js'))
+        .pipe(uglify({
+          compress: true,
+          mangle: true}).on('error', function(err) {
+            util.log(util.colors.red('[Error]'), err.toString());
+            this.emit('end');
+        }))
+        .pipe(header(banner, {pkg: pkg, currentDate: currentDate}))
+        .pipe(gulp.dest(jsDest));
+});
+
+/*
+
+WATCH
+=====
+
+Notes:
+   - browserSync automatically reloads any files
+     that change within the directory it's serving from
+*/
+
+// Run the JS task followed by a reload
+gulp.task('js-watch', ['js'], browserSync.reload);
+
+gulp.task('watch', ['browserSync'], function() {
+
+  gulp.watch(sassSrc, ['styles']);
+  gulp.watch(imgSrc, ['images']);
+  gulp.watch(jsSrc + '/**/*.js', ['js-watch']);
+});
---
+++ src/index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Modern HTML5 Boilerplate</title>
+        <meta name="description" content="YOUR META DESCRIPTION SHOULD BE HERE. This is default meta description, so if you see this, please update!">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/global.css">
+
+        <!-- Touch Icons - iOS and Android 2.1+ 180x180 pixels in size. --> 
+        <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+        
+        <!-- Firefox, Chrome, Safari, IE 11+ and Opera. 192x192 pixels in size. -->
+        <link rel="icon" href="images/favicon.png">
+    </head>
+    <body>
+
+        <header>
+          <h1>@rolle</h1>
+          <img src="images/avatar@2x.png" alt="Avatar of Roni Laukkarinen, a dude with long hair and a goatee, duotone red" />
+        </header>
+
+        <section class="boxes boxes-features">
+          <div class="box box-features">
+            <h2>Gamer features</h2>
+            
+            <div class="features">
+
+              <div class="feature">
+                <header class="heading">
+                  <span class="label">Honesty</span>
+                  <span class="value">99%</span>
+                </header>
+
+                <div class="bar bar-99"><i></i></div>
+              </div>
+
+              <div class="feature">
+                <header class="heading">
+                  <span class="label">Sense of humor</span>
+                  <span class="value">5%</span>
+                </header>
+
+                <div class="bar bar-5"><i></i></div>
+              </div>
+
+              <div class="feature">
+                <header class="heading">
+                  <span class="label">Coding speed</span>
+                  <span class="value">98%</span>
+                </header>
+
+                <div class="bar bar-77"><i></i></div>
+              </div>
+
+              <div class="feature">
+                <header class="heading">
+                  <span class="label">Reading speed</span>
+                  <span class="value">85%</span>
+                </header>
+
+                <div class="bar bar-83"><i></i></div>
+              </div>
+
+              <div class="feature">
+                <header class="heading">
+                  <span class="label">Curiosity</span>
+                  <span class="value">89%</span>
+                </header>
+
+                <div class="bar bar-85"><i></i></div>
+              </div>
+
+              <div class="feature">
+                <header class="heading">
+                  <span class="label">Calmness</span>
+                  <span class="value">82%</span>
+                </header>
+
+                <div class="bar bar-82"><i></i></div>
+              </div>
+
+              <div class="feature">
+                <header class="heading">
+                  <span class="label">The meaning of life</span>
+                  <span class="value">42%</span>
+                </header>
+
+                <div class="bar bar-42"><i></i></div>
+              </div>
+
+              <div class="feature">
+                <header class="heading">
+                  <span class="label">Perfectionism</span>
+                  <span class="value">100%</span>
+                </header>
+
+                <div class="bar bar-100"><i></i></div>
+              </div>
+
+              <div class="feature">
+                <header class="heading">
+                  <span class="label">Humbleness</span>
+                  <span class="value">68%</span>
+                </header>
+
+                <div class="bar bar-68"><i></i></div>
+              </div>
+
+              <div class="feature">
+                <header class="heading">
+                  <span class="label">Stupidity of all graphs like this</span>
+                  <span class="value">100%</span>
+                </header>
+
+                <div class="bar bar-100"><i></i></div>
+              </div>
+
+              <div class="feature">
+                <header class="heading">
+                  <span class="label">Value of any graphcs like this in CV</span>
+                  <span class="value">0%</span>
+                </header>
+
+                <div class="bar bar-1"><i></i></div>
+              </div>
+
+            </div>
+          </div>
+        </section>
+
+        <section class="boxes boxes-activities">
+          <div class="box">
+            <h2>Saying</h2>
+            
+          </div>
+
+          <div class="box">
+            <h2>Reading</h2>
+
+          </div>
+
+          <div class="box">
+            <h2>Watching</h2>
+            
+          </div>
+
+          <div class="box">
+            <h2>Listening</h2>
+            
+          </div>
+
+          <div class="box">
+            <h2>Drinking</h2>
+            
+          </div>
+
+          <div class="box">
+            <h2>Sleeping</h2>
+            
+          </div>
+
+          <div class="box">
+            <h2>Running</h2>
+            
+          </div>
+
+          <div class="box">
+            <h2>Writing</h2>
+            
+          </div>
+
+          <div class="box">
+            <h2>Playing</h2>
+            
+          </div>
+
+          <div class="box">
+            <h2>Working</h2>
+            
+          </div>
+
+          <div class="box">
+            <h2>Coding</h2>
+            
+          </div>
+        </section>
+
+        <section class="boxes boxes-timeline">
+          <div class="box box--wide">
+            <p>Some kind of timeline here</p>
+          </div>
+        </section>
+
+        <section class="boxes boxes-legacy">
+          <div class="box box--wide">
+            <p>Some I don't use any more<br />
+            Older versions of this page,
+          other stuff</p>
+          </div>
+        </section>
+
+        <section class="boxes boxes-contact">
+          <div class="box box--wide">
+            <p>Contact info</p>
+          </div>
+        </section>
+
+        <script src="js/all.js"></script>
+
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-64522118-1');ga('send','pageview');
+        </script>
+
+    </body>
+</html>
