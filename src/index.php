288876991bf4ca5f1b59c27ef02ce81b365e7088
Roni Laukkarinen <roni@laukkarinen.info>
2018-11-13 01:04:04 +0200

    Better front page text

--- src/index.php
+++ src/index.php
@@ -27,7 +27,7 @@
   <header class="block block-hero">
     <div class="content">
       <h1><span>Ron</span> <span>the</span> <span>don.</span></h1>
-      <p>But just say <span>Rolle</span>. My full name is Roni Laukkarinen and I'm a web designer and developer.</p>
+      <p>Just say <span>Rolle</span>. My full name is Roni Laukkarinen and I'm a web designer and developer from Finland.</p>
 
       <img src="images/gravatar.jpg" alt="Gravatar image of me" class="gravatar" />
       <p class="gravatar-description">Yep, that's me in 2010. Still the best photo of me in my opinion.<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 125" width="60" height="60"><path d="M15.8 64.5c9.1 6.6 20.9 8.4 31.6 5.2C58 66.5 67.3 59 73.8 50.3c3.3-4.3 5.9-9 8-14-.3 4.1-.1 8.2.7 12.3.1.6.5 2 1.4 1.6.8-.4.9-2.1.8-2.8-.9-4.7-1-9.4-.2-14.1V33c1-1 .4-5.3-1.2-3.9-2.2 2-4.6 3.8-7.2 5.3-1.3.7-2.7 1.4-4 2-1.4.6-2.8 1.2-4.3 1-.9-.1-1.1 1.7-1.1 2.3 0 .7.2 2.2 1.1 2.3 1.6.2 3.2-.4 4.7-1s2.9-1.3 4.3-2.1c.4-.2.9-.5 1.3-.8-3.6 7.1-8.8 13.5-14.9 18.4-4.3 3.5-9 6.4-14.2 8.3-5.1 1.9-10.6 2.5-16 1.9-5.9-.7-11.6-2.9-16.5-6.4-.8-.6-1.3 1.1-1.4 1.6-.2.7-.1 2 .7 2.6z" fill="#231f20"/></svg></p>
