f9d4e9b32910a2d4ca0be223ea21563f461c1708
Roni Laukkarinen <roni@laukkarinen.info>
2018-11-15 22:25:27 +0200

    Fix home url

--- src/index.php
+++ src/index.php
@@ -14,7 +14,7 @@
 
   <nav class="main-menu">
     <ul>
-      <li><a href="/home" class="active"><?php echo file_get_contents( '../dist/svg/home.svg' ); ?><span>Home</span></a></li>
+      <li><a href="/" class="active"><?php echo file_get_contents( '../dist/svg/home.svg' ); ?><span>Home</span></a></li>
       <li><a href="/blog"><?php echo file_get_contents( '../dist/svg/blog.svg' ); ?><span>Blog</span></a></li>
       <li><a href="/wordpress"><?php echo file_get_contents( '../dist/svg/wordpress.svg' ); ?><span>WordPress</span></a></li>
       <li><a href="/projects"><?php echo file_get_contents( '../dist/svg/projects.svg' ); ?><span>Projects</span></a></li>
