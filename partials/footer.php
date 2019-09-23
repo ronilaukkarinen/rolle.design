0af3331a6194b6611802cbf3f100c68f8a615cf3
Roni Laukkarinen <roni@dude.fi>
2019-09-23 14:52:16 +0300

    Add footer partial

---
+++ partials/footer.php
@@ -0,0 +1,6 @@
+<footer class="block block-my-things">
+  <div class="head">
+    <p>More stuff here soon.</p>
+    <h2><a href="https://github.com/ronilaukkarinen/rolle.design">Follow my progress on GitHub. <?php echo file_get_contents( '../dist/svg/github.svg' ); ?></a></h2>
+  </div>
+</footer>
--- src/now.php
+++ src/now.php
@@ -49,12 +49,7 @@
     </div>
   </header>
 
-  <div class="block block-my-things">
-    <div class="head">
-      <p>More stuff here soon.</p>
-      <h2><a href="https://github.com/ronilaukkarinen/rolle.design">Follow my progress on GitHub. <?php echo file_get_contents( '../dist/svg/github.svg' ); ?></a></h2>
-    </div>
-  </div>
+  <?php include '../partials/nav.php'; ?>
 
   <script src="js/all.js"></script>
 
