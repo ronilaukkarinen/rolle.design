0b68530990fb86dd4657f3c1c62c0a4f29011be4
Roni Laukkarinen <roni@dude.fi>
2021-02-24 10:43:56 +0200

    Update now page

--- src/now.php
+++ src/now.php
@@ -38,13 +38,13 @@
       <p>Now page is a place to tell you about the things I focus on <i><u>right now</u></i>. I'll try to keep this up to date best I can. Inspired by the <a href="https://nownownow.com/">nownownow project</a>.</p>
 
       <ul>
-        <li>Working at <a href="https://www.dude.fi">Dude</a> as a founder and a full-time full stack developer, my tasks consists of back-end, front-end, UI and servers. My code and configs are up to date on <a href="https://github.com/ronilaukkarinen">GitHub</a>.</li>
+        <li>Working at <a href="https://www.dude.fi">Dude</a> as a founder, CTO, and a full-time full stack developer, my tasks consists of front-end, accessibility, UI, servers, a bit of marketing and even back-end. My code and configs are always up to date on <a href="https://github.com/ronilaukkarinen">GitHub</a>.</li>
         <li>Contributing in WordPress community somehow on weekly basis, check out my <a href="https://profiles.wordpress.org/rolle/">WordPress.org profile</a>. Organizing <a href="https://www.meetup.com/Jyvaskyla-WordPress-Meetup/events/upcoming">WordPress Jyväskylä Meetups</a>.</li>
-        <li>Blogging at least once a month about <a href="https://www.kevyttalukemista.fi">books</a>, <a href="https://www.rollemaa.fi">life</a>, <a href="https://www.rollemaa.fi/leffat">movies</a>, <a href="https://www.huurteinen.fi">craft beer</a>, <a href="https://geekylifestyle.com">geeky lifestyle</a>, <a href="https://www.dude.fi/blogi">the web and digital agency business</a> from which the latter is the most active one.</li>
+        <li>Blogging at least once a month about <a href="https://www.kevyttalukemista.fi">books</a>, <a href="https://www.rollemaa.fi">life</a>, <a href="https://www.rollemaa.fi/leffat">movies</a>, <a href="https://geekylifestyle.com">geeky lifestyle</a>, <a href="https://www.dude.fi/blogi">the web and digital agency business</a> from which the latter is the most active one.</li>
         <li>Reading books daily. Occasional progress updates at <a href="http://goodreads.com/rolle">Goodreads</a>.</li>
         <li>Creating dreamy synthwave. Check out <a href="https://soundcloud.com/streetgazer">Streetgazer at SoundCloud</a> or <a href="https://open.spotify.com/artist/1YWN38WUKnCFTssFX0DNbb">Spotify</a>.</li>
         <li><a href="https://twitter.com/rolle">Twitter</a> and <a href="https://www.instagram.com/rolle_">Instagram</a> are my top choices of social media today. Trying to post an Instagram Story every day. Mostly in <a href="https://en.wikipedia.org/wiki/Finnish_language" style="top: 2px; position: relative;" title="Finnish">🇫🇮</a>, sadly.</li>
-        <li class="updated">Updated 23. September, 2019</li>
+        <li class="updated">Updated 24. February, 2021</li>
       </ul>
     </div>
   </header>
