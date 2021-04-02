77dafb10605aad0587bf96a87e2d5344dde9debe
Roni Laukkarinen <roni@dude.fi>
2021-04-02 21:25:06 +0300

    Fix titles on Windows systems

--- content/themes/rolledesign/js/src/block.js
+++ content/themes/rolledesign/js/src/block.js
@@ -3,7 +3,7 @@
  * Duplicate and customize the four lines below for each style variation you'd like to add.
  */
 // eslint-disable-next-line no-undef
-wp.blocks.registerBlockStyle('core/paragraph', {
+wp.blocks.registerBlockStyle('core/heading', {
   name: 'ingress',
   label: 'Ingressi',
 });
