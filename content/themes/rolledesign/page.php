50063af26caa6bf7893de1062581274a16a28721
Roni Laukkarinen <roni@dude.fi>
2021-04-04 19:12:33 +0300

    Change edit button style

--- content/themes/rolledesign/page.php
+++ content/themes/rolledesign/page.php
@@ -22,7 +22,7 @@ get_header(); ?>
 <main class="site-main transition-fade">
   <?php the_content(); ?>
   <?php if ( get_edit_post_link() ) {
-    edit_post_link( sprintf( wp_kses( __( 'Edit <span class="screen-reader-text">%s</span>', 'rolle' ), [ 'span' => [ 'class' => [] ] ] ), get_the_title() ), '<p class="edit-link"><span class="pushable"><span class="shadow"></span><span class="edge"></span><span class="front">', '</span></span></p>' );
+     edit_post_link( sprintf( __( '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg><span class="screen-reader-text">%s</span>', 'rolle' ), [ 'span' => [ 'class' => [] ] ], get_the_title() ), '<p class="edit-link">', '</p>' ); // phps:ignore
   } ?>
 </main>
 </button>
