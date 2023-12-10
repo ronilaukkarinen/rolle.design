e39f264077a5a4a541944885a672c0e894a0608d
Roni Laukkarinen <roni@dude.fi>
2023-12-10 15:09:02 +0200

    Move edit to bottom, remove console.log

--- content/themes/rolledesign/single.php
+++ content/themes/rolledesign/single.php
@@ -29,10 +29,6 @@ get_header(); ?>
         entry_footer();
       ?>
 
-      <?php if ( get_edit_post_link() ) {
-        edit_post_link( sprintf( __( '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg><span class="screen-reader-text">%s</span>', 'rolle' ), [ 'span' => [ 'class' => [] ] ], get_the_title() ), '<p class="edit-link">', '</p>' ); // phps:ignore
-      } ?>
-
       <?php if ( ! empty( $url_to_tweet ) ) : ?>
         <?php
         // If URL contains twitter.com
@@ -63,8 +59,6 @@ get_header(); ?>
 
               // Get ID after the last /
               const postID = postUrl.split("/").pop();
-
-              console.log(postID);
 
               function escapeHtml(unsafe) {
                 return unsafe
@@ -118,7 +112,6 @@ get_header(); ?>
                       commentsWrapper.innerHTML = "";
 
                       descendants.forEach(function(status) {
-                          console.log(descendants)
                         if( status.account.display_name.length > 0 ) {
                           status.account.display_name = escapeHtml(status.account.display_name);
                           status.account.display_name = emojify(status.account.display_name, status.account.emojis);
@@ -252,6 +245,10 @@ get_header(); ?>
 
         <?php endif; ?>
       <?php endif; ?>
+
+      <?php if ( get_edit_post_link() ) {
+        edit_post_link( sprintf( __( '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg><span class="screen-reader-text">%s</span>', 'rolle' ), [ 'span' => [ 'class' => [] ] ], get_the_title() ), '<p class="edit-link">', '</p>' ); // phps:ignore
+      } ?>
 
     </div>
   </section>
